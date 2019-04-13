56f94e7a721de62d0e8989d6aac20517f962afb5
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2019-04-13 18:37:42 +0200

    Creato layout applicazione

--- app/src/main/java/it/unicampania/lsadm/scacchiera/MainActivity.kt
+++ app/src/main/java/it/unicampania/lsadm/scacchiera/MainActivity.kt
@@ -3,6 +3,11 @@ package it.unicampania.lsadm.scacchiera
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Applicazione che realizza un semplice gioco su scacchiere
+ * LSADM 2019
+ * @author Pasquale Cantiello
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
